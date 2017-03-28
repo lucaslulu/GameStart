2d5b0bcd1874089490cfc502a66f62694fb4d4ee
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 18:00:39 -0400

    Update card.cc

--- card.cc
+++ card.cc
@@ -3,6 +3,11 @@
 Card::Card(int cost,string name,string Active,string Trigger)
   : name{name}, cost{cost}, Active{Active}, Trigger{Trigger} {}
   
+void Card::addTarget(shared_ptr<Card> c)
+{
+  target.emplace_back(c);
+}
+
  string Card::getName()
  {
     return name;
