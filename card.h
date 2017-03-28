51c7beb5246b5f0221d84a4f32712615bbf4f2d5
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 18:02:20 -0400

    Update card.h

--- card.h
+++ card.h
@@ -10,18 +10,16 @@ class Card : public Observer
 protected:
 	string name;
 	int cost; // since each card has a cost and a name for sure
-
-	string Active;
-	string Trigger;
-	// if the card does not have the related ability, 
-	// use string "noAbility" to represent
-	vector<shared_ptr<Card>> target;
+	
+	vector<shared_ptr<Card>> target; 
+	// each card has a array of possible target to attack or effect.
 	
 public:
 	void addTarget(shared_ptr<Card> c);
-	Card(int cost,string name,string Active,string Trigger);
+	Card(int cost,string name);
 	virtual ~Card() = default;
 	virtual void useAbility(AbilityType t,Subject &owner) = 0;
+	// the effect differs based on the type of ability it uses.
 	virtual bool isTrigger() = 0;
 	// idea here is that each card is observer of its owner
 
