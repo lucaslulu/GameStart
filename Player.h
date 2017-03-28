7076d8ef3a61dcc6aa688a90ad886b0e453e450a
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 16:41:41 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,16 +1,22 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
 #include "card.h"
+#include "cardbase.h"
+#include "subject.h"
 
-class player
-{
+class player : public subject
+{	
 	int health;
-	vector <minion*> field;
-	vector <minion*> grave;
+	vector <shared_ptr<minion>> field;
+	vector <shared_ptr<minion>> grave;
 	ritual* secret;
-	vector <card*> hand;
+	vector <shared_ptr<card>> hand;
 	public:
 
 	void summon(char);
 	void gotograve(minion);
-
 	
-}
+	
+};
+
+#endif
