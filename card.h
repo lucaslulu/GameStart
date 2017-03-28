9f8b0ee97036b64d5f592a6c456e6aa526b54014
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 18:00:43 -0400

    Update card.h

--- card.h
+++ card.h
@@ -15,8 +15,10 @@ protected:
 	string Trigger;
 	// if the card does not have the related ability, 
 	// use string "noAbility" to represent
+	vector<shared_ptr<Card>> target;
 	
 public:
+	void addTarget(shared_ptr<Card> c);
 	Card(int cost,string name,string Active,string Trigger);
 	virtual ~Card() = default;
 	virtual void useAbility(AbilityType t,Subject &owner) = 0;
