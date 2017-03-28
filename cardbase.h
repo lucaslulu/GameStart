32f137f2e7e0ccdbb8bbba69e88f2c58c4c7d37d
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 16:34:37 -0400

    Update cardbase.h

--- cardbase.h
+++ cardbase.h
@@ -18,6 +18,7 @@ public:
 	int getLife();
 	void takeAtk(int);
 	virtual void useAbility(AbilityType t, Subject &owner) = 0;
+	virtual bool isTrigger() = 0;
 };
 
 class Ritual : public Card
@@ -30,7 +31,7 @@ public:
 	virtual ~Ritual() = 0;
 	int getCharge();
 	virtual void useAbility(AbilityType t, Subject &owner) = 0;
-
+	virtual bool isTrigger() = 0;
 };
 
 class Spell : public Card
@@ -39,12 +40,16 @@ public:
 	Spell(int cost, string name, string Active);
 	virtual ~Spell() = 0;
 	virtual void useAbility(AbilityType t, Subject &owner) = 0;
-	
+	bool isTrigger();
 };
 
 class Enchantment: public Card
 {
-pu
+public:
+	Enchantment(int cost, string name, string Active);
+	virtual ~Enchantment() = 0;
+	bool isTrigger();
+};
 #endif
 
 
