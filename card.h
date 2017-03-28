c218dfaf38f6b317db2ec8e18c8cc0cacd469f99
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 16:13:26 -0400

    Update and rename observer.h to card.h

---
+++ card.h
@@ -0,0 +1,26 @@
+#ifndef _CARD_H_
+#define _CARD_H_
+class Subject;
+class Player;
+
+class Card : public Observer
+{
+protected:
+	string name;
+	int cost; // since each card has a cost and a name for sure
+
+	string Active;
+	string Trigger;
+	// if the card does not have the related ability, 
+	// use string "noAbility" to represent
+	
+public:
+	Card(int cost,string name,string Active,string Trigger);
+	virtual ~Card() = default;
+	virtual void useAbility(AbilityType t,Subject &owner) = 0;
+	virtual bool isTrigger();
+	// idea here is that each card is observer of its owner
+
+	string getName();
+};
+#endif
