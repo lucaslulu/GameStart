c70e4f80417e9c4446f03f7062d42d7843cf3f77
lucaslulu <lucaslulusong@gmail.com>
2017-03-28 16:10:58 -0400

    Update and rename card.h to cardbase.h

--- cardbase.h
+++ cardbase.h
@@ -1,29 +1,10 @@
-#ifndef _CARD_H_
-#define _CARD_H_
+#ifndef _CARDBASE_H_
+#define _CARDBASE_H_
 
 #include <string>
 #include <vector>
+#include "card.h"
 #include "type.h"
-
-class Card : public Observer
-{
-protected:
-	string name;
-	int cost; // since each card has a cost and a name for sure
-
-	string Active;
-	string Trigger;
-	// if the card does not have the related ability, 
-	// use string "noAbility" to represent
-	
-public:
-	Card(int cost,string name,string Active,string Trigger);
-	~Card();
-	virtual void useAbility(AbilityType t,Subject &owner) = 0;
-	// idea here is that each card is observer of its owner
-
-	string getName();
-};
 
 class Minion : public Card
 {
