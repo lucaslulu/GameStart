c3fd1b664c0caa6dcf291ef035d3d53f2150cb99
lucaslulu <lucaslulusong@gmail.com>
2017-03-29 11:13:23 -0400

    Update card.h

--- card.h
+++ card.h
@@ -17,7 +17,7 @@ protected:
 	std::string name;
     std::string description;  // the description of a card's ability
 	int cost; // since each card has a cost and a name for sure
-	std::shared_ptr<Player> opponent;
+	std::shared_ptr<Player> owner;
 	
 	std::vector<std::shared_ptr<Minion>> targetM;
 	std::vector<std::shared_ptr<Ritual>> targetR;
@@ -26,12 +26,12 @@ protected:
 public:
     Card(int cost,std::string name,std::string description = "");
 	void addTarget(std::shared_ptr<Card> c);
-	void addOpponent(std::shared_ptr<Player> p);
+	void addOwner(std::shared_ptr<Player> p);
 	virtual ~Card() = default;
 	virtual CType getType() = 0;
 	//virtual void useAbility(AbilityType t,Subject &owner) = 0;
 	// the effect differs based on the type of ability it uses.
-	virtual bool isTrigger(Player &owner) = 0;
+	virtual bool isTrigger() = 0;
 	// idea here is that each card is observer of its owner
 
 	std::string getName();  // return name of card
